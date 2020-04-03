08d0a390f855e582d6e82b2d5e9716afa59e3d2b
snjsomnath <53001297+snjsomnath@users.noreply.github.com>
2020-04-03 09:21:53 +0200

    plt

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import urllib
 
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
 #time_series_covid19_confirmed_global.csv
 url_confirmed = 'https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv'
 #time_series_covid19_deaths_global.csv
@@ -11,3 +15,10 @@ url_recov = 'https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A
 data_confirmed = pd.read_csv(url_confirmed)
 
 print (data_confirmed)
+
+
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+
