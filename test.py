14c863571132bd9dd4696d7ffc8b9334963b9230
snjsomnath <53001297+snjsomnath@users.noreply.github.com>
2020-04-03 09:18:45 +0200

    Added code

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import urllib
+
+#time_series_covid19_confirmed_global.csv
+url_confirmed = 'https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv'
+#time_series_covid19_deaths_global.csv
+url_death = 'https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_deaths_global.csv&filename=time_series_covid19_deaths_global.csv'
+#time_series_covid19_recovered_global.csv
+url_recov = 'https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_recovered_global.csv&filename=time_series_covid19_recovered_global.csv'
+
+data_confirmed = pd.read_csv(url_confirmed)
+
+print (data_confirmed)
